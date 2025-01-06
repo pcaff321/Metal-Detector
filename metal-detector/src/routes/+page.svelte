1e4efa78087604643db9095aee74ab77f4a46b06
Paul Caffrey <paulcaff2@gmail.com>
2025-01-06 15:51:40 +0000

    Add required key to enter website

--- metal-detector/src/routes/+page.svelte
+++ metal-detector/src/routes/+page.svelte
@@ -12,6 +12,26 @@
         cursor: none;
     }
 
+    .password-entry{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        overflow: hidden;
+    }
+
+    .hint-for-key {
+        height: 40px;
+        padding: 12px;
+    }
+
+    .incorrect-key {
+        color: red;
+        text-align: center;
+        font-weight: 700;
+    }
+
     .heart {
         background-color: red;
         position: absolute;
@@ -109,6 +129,7 @@
 
 <script lang="ts">
     import explosion from '$lib/assets/explosion.mp3';
+  import { onMount } from 'svelte';
     import BrokenMachineContainer from './BrokenMachineContainer.svelte';
     import Needle from './Needle.svelte'
     
@@ -118,6 +139,24 @@
         
         return Math.floor(Math.random() * (max - min)) + min
     }
+
+    let keyGiven = $state(false);
+    let typedKey = $state("");
+    let _exploded = $state(true);
+
+    const KEY = "kinderbueno"
+
+    onMount(() => typedKey = "")
+
+    $effect(() => {
+        if ((encodeURIComponent(typedKey.replace(/\s/g,'').toLowerCase())) === KEY) {
+            keyGiven = true;
+            setTimeout(reset);
+        }
+    });
+
+    const isExploded = () => keyGiven && _exploded;
+    const updateExploded = (exploded: boolean) => _exploded = exploded;
 
     const distance = (x1: number, y1: number, x2: number, y2: number) => Math.hypot(x2 - x1, y2 - y1); 
 
@@ -137,7 +176,7 @@
     let divTop = $state(-1000);
 
     $effect(() => {
-        if (!exploded){
+        if (!isExploded()){
             divLeft = getRandomInteger(0, innerWidth - divWidth);
             divTop = getRandomInteger(20, innerHeight - divHeight);
         }
@@ -160,7 +199,7 @@
 
     $effect(() => {
         
-        if (!exploded) {
+        if (!isExploded()) {
             const hearts = document.getElementsByClassName('heart');
 
             for (let i = 0; i < hearts.length; i++) {
@@ -182,7 +221,7 @@
     });
 
     const playBeep = () => {
-        if (exploded){
+        if (isExploded() || !keyGiven){
             return;
         }
         const audioCtx = new AudioContext();
@@ -207,7 +246,6 @@
     let animationFrameId: number;
 
     let extremeBeepDuration = $state(0)
-    let exploded = $state(false);
 
     const tick = $derived((currentTime: number) => {
 
@@ -228,12 +266,16 @@
   
   const EXTREME_BEEP_DURATION_THRESHOLD = 5000;
 
-  function repair(e: Event) {
-    exploded = false;
+  function reset(){
+    updateExploded(false);
     extremeBeepDuration = 0;
     divLeft = -50000;
     divTop = -5000;
     setBeepInterval(MAX_INTERVAL);
+  }
+
+  function repair(e: Event) {
+    reset();
     
     const clientX = (e as MouseEvent).clientX;
     const clientY = (e as MouseEvent).clientY;
@@ -243,8 +285,8 @@
   }
 
   $effect(() => {
-    if (extremeBeepDuration >= EXTREME_BEEP_DURATION_THRESHOLD && !exploded){
-        exploded = true;
+    if (extremeBeepDuration >= EXTREME_BEEP_DURATION_THRESHOLD && !isExploded()){
+        updateExploded(true);
         explosionAudio?.play()
         cancelAnimationFrame(animationFrameId);
     }
@@ -299,7 +341,7 @@
     }
 
     $effect(() => {
-        if (!exploded) {
+        if (!isExploded()) {
             lastBeepTime = performance.now();
             animationFrameId = requestAnimationFrame(tick);
         }
@@ -315,28 +357,40 @@
 <!-- svelte-ignore a11y_no_static_element_interactions -->
 <!-- svelte-ignore a11y_mouse_events_have_key_events -->
 <!-- svelte-ignore event_directive_deprecated -->
-{#if exploded}
-    <BrokenMachineContainer {repair} />
-{:else}
-<div class="container {withinImage ? 'shakeContainerExtreme' : ''}" on:mousemove|preventDefault={handleMousemove}>
-    <h1>
-        Cuteness Detector
-    </h1>
-    <Needle needle_intensity={(1 - degree_of_closeness) * 100} withinImage={withinImage} />
-    {#if innerWidth > 0}
-    {#await import(`$lib/assets/sevgi${getRandomInteger(1, 7)}.png`) then { default: src }}
-    <img class="image" style="{exploded ? 'display: none' : ''}" style:top={`${divTop}px`} style:left={`${divLeft}px`} {src} alt="Cutie Pie" />
-    {/await}
+{#if keyGiven}
+    {#if isExploded()}
+        <BrokenMachineContainer {repair} />
+    {:else}
+    <div class="container {withinImage ? 'shakeContainerExtreme' : ''}" on:mousemove|preventDefault={handleMousemove}>
+        <h1>
+            Cuteness Detector
+        </h1>
+        <Needle needle_intensity={(1 - degree_of_closeness) * 100} withinImage={withinImage} />
+        {#if innerWidth > 0}
+        {#await import(`$lib/assets/sevgi${getRandomInteger(1, 7)}.png`) then { default: src }}
+        <img class="image" style="{isExploded() ? 'display: none' : ''}" style:top={`${divTop}px`} style:left={`${divLeft}px`} {src} alt="Cutie Pie" />
+        {/await}
+        {/if}
+
+        {#each { length: 169 }}
+            <div class="heart"></div>
+        {/each}
+
+        <div class="detector {withinImage ? 'shakeContainerExtreme' : ''}" style:top={`${cursorTop}px`} style:left={`${cursorLeft - 20}px`} ><svg height="40" width="40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" xml:space="preserve">
+            <path d="M418.254 179.2c-9.387 0-18.773 3.413-25.6 8.533V179.2c0-23.893-18.773-42.667-42.667-42.667-9.387 0-18.773 3.413-25.6 8.533 0-23.893-18.773-42.667-42.667-42.667-9.387 0-18.773 3.413-25.6 8.533V42.667C256.121 18.773 237.347 0 213.454 0s-42.667 18.773-42.667 42.667v193.707c-18.773-32.427-62.293-48.64-95.573-36.693-4.267.853-7.68 3.413-11.947 5.973-12.8 8.533-11.947 19.627-11.947 23.04-.853 5.12 0 5.973 11.947 19.627 14.507 17.067 46.08 52.907 65.707 88.747 1.707 1.707 24.747 39.253 50.347 67.413v22.187c0 46.933 38.4 85.333 85.333 85.333h85.333c36.693 0 75.093-30.72 83.627-66.56 2.56-10.24 5.12-17.92 8.533-21.333 8.533-10.24 18.773-34.133 18.773-65.707V221.867c.001-23.894-18.773-42.667-42.666-42.667zm-67.413 315.733h-85.333c-37.547 0-68.267-30.72-68.267-68.267v-6.767c2.875 2.339 5.76 4.61 8.533 6.767 3.413 3.413 7.68 6.827 11.947 10.24 1.707.853 3.413 1.707 5.12 1.707 2.56 0 5.12-.853 6.827-3.413 3.413-3.413 2.56-8.533-.853-11.947-4.267-3.413-8.533-6.827-12.8-10.24-7.494-5.829-14.983-11.664-21.684-18.285-25.475-26.456-49.996-66.195-49.996-66.195-20.48-36.693-52.907-73.387-68.267-91.307-2.56-3.413-5.973-7.68-7.68-9.387 0-2.56 0-4.267 5.973-7.68 3.413-1.707 5.973-3.413 8.533-4.267 22.187-7.68 58.88 2.56 75.093 30.72l13.191 19.786c.109.523.26 1.041.462 1.547l8.533 17.067c1.707 3.413 10.24 5.12 11.093 3.413 3.413-1.707 5.12-6.827 3.413-11.093l-6.02-12.039c.028-.253.046-.507.046-.761V42.667c0-14.507 11.093-25.6 25.6-25.6s25.6 11.093 25.6 25.6V284.16c0 5.12 3.413 8.533 8.533 8.533s8.533-3.413 8.533-8.533V145.067c0-14.507 11.093-25.6 25.6-25.6s25.6 11.093 25.6 25.6v130.56c0 5.12 3.413 8.533 8.533 8.533s8.533-3.413 8.533-8.533V179.2c0-14.507 11.093-25.6 25.6-25.6 14.507 0 25.6 11.093 25.6 25.6V284.16c0 5.12 3.413 8.533 8.533 8.533s8.533-3.413 8.533-8.533v-62.293c0-14.507 11.093-25.6 25.6-25.6 14.507 0 25.6 11.093 25.6 25.6V358.4c0 15.106-2.777 27.428-6.161 36.621-1.015.932-1.826 2.12-2.372 3.485-4.267 12.8-16.213 17.067-37.547 23.04-4.267.853-6.827 5.973-5.973 10.24 1.707 3.413 5.12 5.973 8.533 5.973.853 0 1.707 0 1.707.853 5.648-1.521 11.128-3.098 16.289-4.954-.659 2.39-1.258 4.895-1.782 7.514-5.97 29.015-37.543 53.761-66.556 53.761z"/>
+        </svg>
+        </div>
+    </div>
     {/if}
-
-    {#each { length: 169 }}
-        <div class="heart"></div>
-    {/each}
-
-    <div class="detector {withinImage ? 'shakeContainerExtreme' : ''}" style:top={`${cursorTop}px`} style:left={`${cursorLeft - 20}px`} ><svg height="40" width="40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" xml:space="preserve">
-        <path d="M418.254 179.2c-9.387 0-18.773 3.413-25.6 8.533V179.2c0-23.893-18.773-42.667-42.667-42.667-9.387 0-18.773 3.413-25.6 8.533 0-23.893-18.773-42.667-42.667-42.667-9.387 0-18.773 3.413-25.6 8.533V42.667C256.121 18.773 237.347 0 213.454 0s-42.667 18.773-42.667 42.667v193.707c-18.773-32.427-62.293-48.64-95.573-36.693-4.267.853-7.68 3.413-11.947 5.973-12.8 8.533-11.947 19.627-11.947 23.04-.853 5.12 0 5.973 11.947 19.627 14.507 17.067 46.08 52.907 65.707 88.747 1.707 1.707 24.747 39.253 50.347 67.413v22.187c0 46.933 38.4 85.333 85.333 85.333h85.333c36.693 0 75.093-30.72 83.627-66.56 2.56-10.24 5.12-17.92 8.533-21.333 8.533-10.24 18.773-34.133 18.773-65.707V221.867c.001-23.894-18.773-42.667-42.666-42.667zm-67.413 315.733h-85.333c-37.547 0-68.267-30.72-68.267-68.267v-6.767c2.875 2.339 5.76 4.61 8.533 6.767 3.413 3.413 7.68 6.827 11.947 10.24 1.707.853 3.413 1.707 5.12 1.707 2.56 0 5.12-.853 6.827-3.413 3.413-3.413 2.56-8.533-.853-11.947-4.267-3.413-8.533-6.827-12.8-10.24-7.494-5.829-14.983-11.664-21.684-18.285-25.475-26.456-49.996-66.195-49.996-66.195-20.48-36.693-52.907-73.387-68.267-91.307-2.56-3.413-5.973-7.68-7.68-9.387 0-2.56 0-4.267 5.973-7.68 3.413-1.707 5.973-3.413 8.533-4.267 22.187-7.68 58.88 2.56 75.093 30.72l13.191 19.786c.109.523.26 1.041.462 1.547l8.533 17.067c1.707 3.413 10.24 5.12 11.093 3.413 3.413-1.707 5.12-6.827 3.413-11.093l-6.02-12.039c.028-.253.046-.507.046-.761V42.667c0-14.507 11.093-25.6 25.6-25.6s25.6 11.093 25.6 25.6V284.16c0 5.12 3.413 8.533 8.533 8.533s8.533-3.413 8.533-8.533V145.067c0-14.507 11.093-25.6 25.6-25.6s25.6 11.093 25.6 25.6v130.56c0 5.12 3.413 8.533 8.533 8.533s8.533-3.413 8.533-8.533V179.2c0-14.507 11.093-25.6 25.6-25.6 14.507 0 25.6 11.093 25.6 25.6V284.16c0 5.12 3.413 8.533 8.533 8.533s8.533-3.413 8.533-8.533v-62.293c0-14.507 11.093-25.6 25.6-25.6 14.507 0 25.6 11.093 25.6 25.6V358.4c0 15.106-2.777 27.428-6.161 36.621-1.015.932-1.826 2.12-2.372 3.485-4.267 12.8-16.213 17.067-37.547 23.04-4.267.853-6.827 5.973-5.973 10.24 1.707 3.413 5.12 5.973 8.533 5.973.853 0 1.707 0 1.707.853 5.648-1.521 11.128-3.098 16.289-4.954-.659 2.39-1.258 4.895-1.782 7.514-5.97 29.015-37.543 53.761-66.556 53.761z"/>
-      </svg>
-      </div>
+{:else}
+<div class="password-entry">
+    <h1>Heeeelllllllloooooooooooooo please enter correct password:</h1>
+    <input type="text" class="password-input" bind:value={typedKey} placeholder={"ENTER KEY"} />
+    <div class="hint-for-key">
+        {#if typedKey !== ""}
+        <span class="incorrect-key">THAT IS FUCKING INCORRECT</span>
+        {/if}
+    </div>
 </div>
 {/if}
 
